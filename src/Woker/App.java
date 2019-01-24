bd0a077b4b8a36ef5bc7ae332c5498ebb96c91f6
bothisattva <orlov21423@mail.ru>
2019-01-24 12:41:01 +0300

    lesson5
    * Создать класс "Сотрудник" с полями: ФИО, должность, email, телефон, зарплата, возраст;
    * Конструктор класса должен заполнять эти поля при создании объекта;
    * Внутри класса «Сотрудник» написать метод, который выводит информацию об объекте в консоль;

---
+++ src/Woker/App.java
@@ -0,0 +1,9 @@
+package Woker;
+
+public class App {
+    public static void main(String[] args) {
+    woker woker1 = new woker(" 1123  "," 2 ","3 ","4 ","5 ",8);
+    woker1.EmployeeInformation();
+
+    }
+}
