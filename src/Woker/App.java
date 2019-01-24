f181ec6629b5a3bbe5055c307d9bd0ef507e9c5f
bothisattva <orlov21423@mail.ru>
2019-01-24 13:03:01 +0300

    l5.2
    создан массив сотрудников

--- src/Woker/App.java
+++ src/Woker/App.java
@@ -2,8 +2,15 @@ package Woker;
 
 public class App {
     public static void main(String[] args) {
-    woker woker1 = new woker(" 1123  "," 2 ","3 ","4 ","5 ",8);
-    woker1.EmployeeInformation();
+        woker [] wokersArray = new woker[5];
+  //  woker woker1 = new woker(" 1123  "," 2 ","3 ","4 ","5 ",8);
+    //woker1.EmployeeInformation();
+    wokersArray [0] = new  woker (" 1  "," 2 ","3 ","4 ","5 ",30);
+    wokersArray [1] = new  woker (" 2  "," 2 ","3 ","4 ","5 ",41);
+    wokersArray [2] = new  woker (" 3  "," 2 ","3 ","4 ","5 ",42);
+    wokersArray [3] = new  woker (" 4  "," 2 ","3 ","4 ","5 ",20);
+    wokersArray [4] = new  woker (" 5  "," 2 ","3 ","4 ","5 ",23);
+
 
     }
 }
--- src/Woker/woker.java
+++ src/Woker/woker.java
@@ -19,6 +19,8 @@ public class woker {
 
         
     }
+
+
     public  void EmployeeInformation(){
         System.out.println( FollName + Post + Mail + phone + salary +  ege);
     }
